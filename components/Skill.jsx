90436ce14d3a4c4870d8ba5fff4242ac987b3de8
muratcan-yuksel <muratcanyukselpro@gmail.com>
2022-11-29 18:09:51 +0300

    add skills

--- components/Skill.jsx
+++ components/Skill.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import circle from "../assets/circle.svg";
 
-const Skill = () => {
-  return <div>Skill</div>;
+const Skill = ({ directionLeft }) => {
+  return (
+    <div className="group relative flex cursor-pointer m-10">
+      {/* this animation is problematic on mobile */}
+      <motion.div
+        initial={{ x: directionLeft ? -200 : 200, opacity: 0 }}
+        transition={{ duration: 1 }}
+        whileInView={{ opacity: 1, x: 0 }}
+      >
+        <Image
+          src={circle}
+          alt=""
+          className="rounded-full border border-gray-500 object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        />
+      </motion.div>
+      {/* here I omit the skill percentage section, because it's stupid */}
+    </div>
+  );
 };
 
 export default Skill;
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,7 +1,31 @@
 import React from "react";
-
+import { motion } from "framer-motion";
+import Skill from "./Skill";
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className="h-screen  flex flex-col ">
+      <h3 className=" uppercase my-10 tracking-[20px] text-gray-500 text-2xl text-center">
+        Skills
+      </h3>
+      <div className="flex flex-wrap justify-center">
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
